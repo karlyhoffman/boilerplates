d03d576ddabec1a0d8b865b74f8b323fa8b49205
Karly <karlyhoffman@gmail.com>
2018-03-22 06:14:38 -0500

    convert to ES6

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -13,30 +13,31 @@
   'use strict';
 
   $(function () {
+    var _this = this;
 
     // Reveal animations
     var controller = new ScrollMagic.Controller();
 
     $(".reveal").each(function (index, elem) {
-      var revealIn = TweenMax.fromTo(this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop: 0, ease: Power0.easeNone });
-      var revealScene = new ScrollMagic.Scene({ triggerHook: "0.72", triggerElement: this }).setTween(revealIn).addTo(controller);
+      var revealIn = TweenMax.fromTo(_this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop: 0, ease: Power0.easeNone });
+      var revealScene = new ScrollMagic.Scene({ triggerHook: "0.72", triggerElement: _this }).setTween(revealIn).addTo(controller);
     });
 
     $(".fade").each(function (index, elem) {
-      var fadeIn = TweenMax.fromTo(this, 0.5, { "opacity": "0" }, { "opacity": "1", ease: Power0.easeNone });
-      var fadeScene = new ScrollMagic.Scene({ triggerHook: "0.95", triggerElement: this }).setTween(fadeIn).addTo(controller);
+      var fadeIn = TweenMax.fromTo(_this, 0.5, { "opacity": "0" }, { "opacity": "1", ease: Power0.easeNone });
+      var fadeScene = new ScrollMagic.Scene({ triggerHook: "0.95", triggerElement: _this }).setTween(fadeIn).addTo(controller);
     });
 
     $(".reveal-stagger").each(function (index, elem) {
       var animStaggerSubjects = $(elem).find('.reveal-elmt');
       var fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.3, { "opacity": "0", marginTop: 20 }, { "opacity": "1", marginTop: 0, ease: Power2.easeOut }, 0.2);
-      var revealStaggerScene = new ScrollMagic.Scene({ triggerHook: "0.75", triggerElement: this }).setTween(fadeStaggerIn).addTo(controller);
+      var revealStaggerScene = new ScrollMagic.Scene({ triggerHook: "0.75", triggerElement: _this }).setTween(fadeStaggerIn).addTo(controller);
     });
 
     $(".stagger-x").each(function (index, elem) {
       var animStaggerSubjects = $(elem).find('.x-elmt');
       var fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.4, { "opacity": "0", marginLeft: -200 }, { "opacity": "1", marginLeft: -5, ease: Power2.easeOut }, 0.2);
-      var revealStaggerScene = new ScrollMagic.Scene({ triggerHook: "0.75", triggerElement: this }).setTween(fadeStaggerIn).addTo(controller);
+      var revealStaggerScene = new ScrollMagic.Scene({ triggerHook: "0.75", triggerElement: _this }).setTween(fadeStaggerIn).addTo(controller);
     });
 
     $(".line-cont").each(function (index, elem) {
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,34 +5,34 @@
   $(function () {
 
     // Reveal animations
-    var controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller();
 
-    $( ".reveal" ).each(function( index, elem ) {
-     var revealIn = TweenMax.fromTo(this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop:0, ease: Power0.easeNone });
-     var revealScene = new ScrollMagic.Scene({triggerHook: "0.72", triggerElement: this}).setTween(revealIn).addTo(controller);
+    $( ".reveal" ).each(( index, elem )=> {
+     const revealIn = TweenMax.fromTo(this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop:0, ease: Power0.easeNone });
+     const revealScene = new ScrollMagic.Scene({triggerHook: "0.72", triggerElement: this}).setTween(revealIn).addTo(controller);
     });
 
-    $( ".fade" ).each(function( index, elem ) {
-     var fadeIn = TweenMax.fromTo(this, 0.5, {"opacity": "0"}, {"opacity": "1", ease: Power0.easeNone});
-     var fadeScene = new ScrollMagic.Scene({triggerHook: "0.95", triggerElement: this}).setTween(fadeIn).addTo(controller);
+    $( ".fade" ).each(( index, elem )=> {
+     const fadeIn = TweenMax.fromTo(this, 0.5, {"opacity": "0"}, {"opacity": "1", ease: Power0.easeNone});
+     const fadeScene = new ScrollMagic.Scene({triggerHook: "0.95", triggerElement: this}).setTween(fadeIn).addTo(controller);
     });
 
-    $( ".reveal-stagger" ).each(function( index, elem ) {
-     var animStaggerSubjects = $(elem).find('.reveal-elmt');
-     var fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.3, {"opacity": "0", marginTop:20}, {"opacity": "1", marginTop:0, ease: Power2.easeOut}, 0.2);
-     var revealStaggerScene = new ScrollMagic.Scene({triggerHook: "0.75", triggerElement: this}).setTween(fadeStaggerIn).addTo(controller);
+    $( ".reveal-stagger" ).each(( index, elem )=> {
+     const animStaggerSubjects = $(elem).find('.reveal-elmt');
+     const fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.3, {"opacity": "0", marginTop:20}, {"opacity": "1", marginTop:0, ease: Power2.easeOut}, 0.2);
+     const revealStaggerScene = new ScrollMagic.Scene({triggerHook: "0.75", triggerElement: this}).setTween(fadeStaggerIn).addTo(controller);
     });
 
-    $( ".stagger-x" ).each(function( index, elem ) {
-     var animStaggerSubjects = $(elem).find('.x-elmt');
-     var fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.4, {"opacity": "0", marginLeft:-200}, {"opacity": "1", marginLeft:-5, ease: Power2.easeOut}, 0.2);
-     var revealStaggerScene = new ScrollMagic.Scene({triggerHook: "0.75", triggerElement: this}).setTween(fadeStaggerIn).addTo(controller);
+    $( ".stagger-x" ).each(( index, elem )=> {
+     const animStaggerSubjects = $(elem).find('.x-elmt');
+     const fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.4, {"opacity": "0", marginLeft:-200}, {"opacity": "1", marginLeft:-5, ease: Power2.easeOut}, 0.2);
+     const revealStaggerScene = new ScrollMagic.Scene({triggerHook: "0.75", triggerElement: this}).setTween(fadeStaggerIn).addTo(controller);
     });
 
-    $( ".line-cont" ).each(function( index, elem ) {
-      var lineAnim = $(elem).find('.line');
-      var lineIn = TweenMax.fromTo(lineAnim, 2, {"height": "0"}, {"height": "200%", ease: Power0.easeNone});
-      var revealLine = new ScrollMagic.Scene({triggerHook: "0.7", triggerElement: elem}).setTween(lineIn).addTo(controller);
+    $( ".line-cont" ).each(( index, elem )=> {
+      const lineAnim = $(elem).find('.line');
+      const lineIn = TweenMax.fromTo(lineAnim, 2, {"height": "0"}, {"height": "200%", ease: Power0.easeNone});
+      const revealLine = new ScrollMagic.Scene({triggerHook: "0.7", triggerElement: elem}).setTween(lineIn).addTo(controller);
     });
 
   });
